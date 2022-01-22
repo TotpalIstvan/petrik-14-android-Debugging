21e7b209fc6fee9800a90da3a9736b1e4926ea59
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-22 21:29:02 +0100

    Menu

--- app/src/main/java/com/example/debugging/ThirdActivity.java
+++ app/src/main/java/com/example/debugging/ThirdActivity.java
@@ -19,7 +19,7 @@ public class ThirdActivity extends AppCompatActivity {
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ThirdActivity.this,MainActivity.class);
+                Intent intent = new Intent(ThirdActivity.this,Menu.class);
                 startActivity(intent);
                 finish();
             }
